480fb21393681ef3d69f1de4a2545a908a38798a
masonawolfe <49623146+masonawolfe@users.noreply.github.com>
2026-03-14 14:46:44 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DownfieldOS from './DownfieldOS.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DownfieldOS />
+  </React.StrictMode>,
+)
